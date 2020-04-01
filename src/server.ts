5a1239356dcb47f2e4658c41ca9217ebf3087c1f
Leonardo Isso <leoisso.work@gmail.com>
2020-04-01 20:27:03 -0300

    Testing environment variables and Requester

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import './utils/env'
+import Requester from './Requests/Requester'
+
+const a = Requester
+
+const request = async (): Promise<object> => {
+  const response = await a.get('/statistics')
+  return response
+}
+
+request().then(res => console.log(res))
