ea7983c75fc42be77a035af8fecbb68aaaec7b7c
Leonardo Isso <leoisso.work@gmail.com>
2020-04-01 23:52:57 -0300

    Update tests

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,17 @@
 import './utils/env'
 import { StatisticsRequestMaker } from './Requests/RequestMakers'
+import StatisticsService from './Services/StatisticsService'
+import MessageService from './Services/MessageService/MessageService'
 
-const request = async (): Promise<object> => {
+const handleStatistic = async ():Promise<string> => {
   const statisticRequest = new StatisticsRequestMaker()
   await statisticRequest.makeRequest()
-  return statisticRequest.getResponseData()
+  const payload = statisticRequest.getResponseData()
+  const statisticsService = new StatisticsService(payload)
+  const statisticData = statisticsService.getStatisticData()
+  return MessageService.createStatisticsMessage(statisticData)
 }
 
-const extractAll = async () => {
-  const response = await request()
-  const allData = response.response.find(data => data.country === 'All')
-  return allData
-}
-
-const createMessage = async () => {
-  const data = await extractAll()
-  return `
-  Last Update: ${data.time}
-  Total Cases: ${data.cases.total}
-  Active Cases: ${data.cases.active}
-  Recovered Cases: ${data.cases.recovered}
-  Critical Cases: ${data.cases.critical}
-  Deaths: ${data.deaths.total}
-  `
-}
-
-createMessage().then(a => {
+handleStatistic().then(a => {
   console.log(a)
 })
