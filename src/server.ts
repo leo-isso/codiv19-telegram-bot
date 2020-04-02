b41aca33a3ea06873dcbfe32d843e7478e8caf11
Leonardo Isso <leoisso.work@gmail.com>
2020-04-01 22:21:28 -0300

    Test roadmap

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
 import './utils/env'
-import Requester from './Requests/Requester'
-
-const a = Requester.getInstance()
+import { StatisticsRequestMaker } from './Requests/RequestMakers'
 
 const request = async (): Promise<object> => {
-  const response = await a.get('/statistics')
-  return response
+  const statisticRequest = new StatisticsRequestMaker()
+  await statisticRequest.makeRequest()
+  return statisticRequest.getResponseData()
 }
 
-request().then(res => console.log(res))
+const extractAll = async () => {
+  const response = await request()
+  const allData = response.response.find(data => data.country === 'All')
+  return allData
+}
+
+const createMessage = async () => {
+  const data = await extractAll()
+  return `
+  Last Update: ${data.time}
+  Total Cases: ${data.cases.total}
+  Active Cases: ${data.cases.active}
+  Recovered Cases: ${data.cases.recovered}
+  Critical Cases: ${data.cases.critical}
+  Deaths: ${data.deaths.total}
+  `
+}
+
+createMessage().then(a => {
+  console.log(a)
+})
