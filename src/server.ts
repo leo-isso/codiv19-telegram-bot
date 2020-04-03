87d2dbd904d035443b68a1761a6f0f2130a6ed51
Leonardo Isso <leoisso.work@gmail.com>
2020-04-03 02:33:20 -0300

    Simulate Invoker

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,11 @@
-import TelegramBot from 'node-telegram-bot-api'
-
 import './utils/env'
-import { StatisticsRequestMaker } from './Requests/RequestMakers'
-import StatisticsService from './Services/StatisticsService'
-import MessageService from './Services/MessageService/MessageService'
 
-const handleStatistic = async ():Promise<string> => {
-  const statisticRequest = new StatisticsRequestMaker()
-  await statisticRequest.makeRequest()
-  const payload = statisticRequest.getResponseData()
-  const statisticsService = new StatisticsService(payload)
-  const statisticData = statisticsService.getStatisticData()
-  return MessageService.createStatisticsMessage(statisticData)
+import { SendHourlyMessageToChannelCommand } from './TelegramBot/Commands'
+import { TelegramBotReceiver } from './TelegramBot/Receivers'
+
+const executeCommand = () :void => {
+  const command = new SendHourlyMessageToChannelCommand(new TelegramBotReceiver())
+  command.execute()
 }
 
-const token = process.env.TELEGRAM_BOT_TOKEN
-const bot = new TelegramBot(token, { polling: true })
-
-bot.onText(/\/global/, async (msg) => {
-  const chatId = msg.chat.id
-  const response = await handleStatistic()
-  bot.sendMessage(chatId, response)
-})
+executeCommand()
