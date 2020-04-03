b346f4e1285d3b5d6d7c8bf932feead6ce819cea
Leonardo Isso <leoisso.work@gmail.com>
2020-04-03 03:32:18 -0300

    Fix cron script

--- scripts/cron-setup.sh
+++ scripts/cron-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-printf "0 * * * * echo 'entry' >> node .\/server.js\n" | crontab -
+printf "0 * * * * /usr/local/bin/node /covid19-telegram-bot/dist/server.js\n" | crontab -
 /etc/init.d/cron start
