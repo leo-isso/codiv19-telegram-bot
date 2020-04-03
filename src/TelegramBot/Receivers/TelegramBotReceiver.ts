4439efb74cf09c5bd69245569f8823ec53f37709
Leonardo Isso <leoisso.work@gmail.com>
2020-04-03 02:25:13 -0300

    Implement sendHourlyMessageToChannel and createHourlyMessage

--- src/TelegramBot/Receivers/TelegramBotReceiver.ts
+++ src/TelegramBot/Receivers/TelegramBotReceiver.ts
@@ -1,5 +1,9 @@
 import TelegramBot from 'node-telegram-bot-api'
 
+import { StatisticsRequestMaker } from '../../Requests/RequestMakers'
+import StatisticsService from '../../Services/StatisticsService'
+import MessageService from '../../Services/MessageService/MessageService'
+
 class TelegramBotReceiver {
   public telegramBot: TelegramBot
   private channelId: string = process.env.TELEGRAM_CHANNEL_ID
@@ -8,8 +12,22 @@ class TelegramBotReceiver {
     this.telegramBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN)
   }
 
-  sendMessageToChannel (message: string):void {
+  async sendHourlyMessageToChannel (): Promise<void> {
+    const message = await this.createHourlyMessage()
     this.telegramBot.sendMessage(this.channelId, message)
+  }
+
+  async createHourlyMessage (): Promise<string> {
+    const statisticRequest = new StatisticsRequestMaker()
+    await statisticRequest.makeRequest()
+    const payload = statisticRequest.getResponseData()
+
+    const statisticsService = new StatisticsService(payload)
+    const statisticData = statisticsService.getStatisticData()
+
+    const message = MessageService.createStatisticsMessage(statisticData)
+
+    return message
   }
 }
 
