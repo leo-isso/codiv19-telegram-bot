d5d05fddd1641b7d12a0a5ab2433faaa5af6aded
Leonardo Isso <leoisso.work@gmail.com>
2020-04-04 22:25:04 -0300

    Split numbers into mil

--- src/Services/MessageService/MessageService.ts
+++ src/Services/MessageService/MessageService.ts
@@ -1,16 +1,18 @@
 import { CountrySchema } from '../../Schemas'
+import MessageFormater from './MessageFormaterService'
 
 class MessageService {
   static createStatisticsMessage (statisticData: CountrySchema): string {
+    const messageFormater = new MessageFormater()
     const dateTime = new Date(statisticData.time).toLocaleString('us-en', { timeZoneName: 'short' })
     return `
 *COVID-19 - Hourly Update*
 
-*Total Cases:* ${statisticData.cases.total}
-*Active Cases:* ${statisticData.cases.active}
-*Recovered Cases:* ${statisticData.cases.recovered}
-*Critical Cases:* ${statisticData.cases.critical}
-*Deaths:* ${statisticData.deaths.total}
+*Total Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.total.toString())}
+*Active Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.active.toString())}
+*Recovered Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.recovered.toString())}
+*Critical Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.critical.toString())}
+*Deaths:* ${messageFormater.addSpaceToBigStringNumber(statisticData.deaths.total.toString())}
 
 *Updated @* ${dateTime}
     `
