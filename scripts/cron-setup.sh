924309b29357b38bfb9f7b616f6ba39aafc255ed
Leonardo Isso <leoisso.work@gmail.com>
2020-04-04 23:07:46 -0300

    Update cron script

--- scripts/cron-setup.sh
+++ scripts/cron-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-printf "0 * * * * /usr/local/bin/node /covid19-telegram-bot/dist/server.js\n" | crontab -
+printf "5 * * * * /usr/local/bin/node /covid19-telegram-bot/dist/server.js --hourly\n" | crontab -
 /etc/init.d/cron start
