13d2dc4532575f69c024ec052bc040043152bed9
Leonardo Isso <leoisso.work@gmail.com>
2020-04-05 04:18:52 -0300

    Update MessageService

--- src/Services/MessageService/MessageService.ts
+++ src/Services/MessageService/MessageService.ts
@@ -3,40 +3,37 @@ import MessageFormater from './MessageFormaterService'
 
 class MessageService {
   static createStatisticsMessage (statisticData: CountrySchema): string {
-    const messageFormater = new MessageFormater()
+    const messageBody = this.createCountryData(statisticData, false)
     const dateTime = new Date(statisticData.time).toLocaleString('us-en', { timeZoneName: 'short' })
     return `
 *COVID-19 - Hourly Update*
-
-*Total Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.total.toString())}
-*Active Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.active.toString())}
-*Recovered Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.recovered.toString())}
-*Critical Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.critical.toString())}
-*Deaths:* ${messageFormater.addSpaceToBigStringNumber(statisticData.deaths.total.toString())}
-
+${messageBody}
 *Updated @* ${dateTime}
     `
   }
 
   static createTopFiveMessage (statisticData: CountrySchema[]): string {
-    const messageFormater = new MessageFormater()
     const dateTime = new Date(statisticData[0].time).toLocaleString('us-en', { timeZoneName: 'short' })
-
     const messageBody = statisticData.map(countryData => {
-      return `*${countryData.country}*
-*Total Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.total.toString())}
-*Active Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.active.toString())}
-*Recovered Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.recovered.toString())}
-*Critical Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.critical.toString())}
-*Deaths:* ${messageFormater.addSpaceToBigStringNumber(countryData.deaths.total.toString())}
-      `
-    }).join('\n\n')
+      return this.createCountryData(countryData, true)
+    }).join('')
 
     return `
 *COVID-19 - TOP 5 most affected countries*
-
 ${messageBody}
 *Updated @* ${dateTime}
+    `
+  }
+
+  static createCountryData (statisticData: CountrySchema, hasCountryName: boolean): string {
+    const messageFormater = new MessageFormater()
+    return `
+${hasCountryName ? `*${statisticData.country}*` : ''}
+*Total Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.total.toString())}
+*Active Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.active.toString())}
+*Recovered Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.recovered.toString())}
+*Critical Cases:* ${messageFormater.addSpaceToBigStringNumber(statisticData.cases.critical.toString())}
+*Deaths:* ${messageFormater.addSpaceToBigStringNumber(statisticData.deaths.total.toString())}
     `
   }
 }
