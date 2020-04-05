66467789ab12c8f5e06c8020b9bbe0969232eb62
Leonardo Isso <leoisso.work@gmail.com>
2020-04-05 02:46:22 -0300

    Create Top Five message

--- src/Services/MessageService/MessageService.ts
+++ src/Services/MessageService/MessageService.ts
@@ -17,6 +17,28 @@ class MessageService {
 *Updated @* ${dateTime}
     `
   }
+
+  static createTopFiveMessage (statisticData: CountrySchema[]): string {
+    const messageFormater = new MessageFormater()
+    const dateTime = new Date(statisticData[0].time).toLocaleString('us-en', { timeZoneName: 'short' })
+
+    const messageBody = statisticData.map(countryData => {
+      return `
+*Total Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.total.toString())}
+*Active Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.active.toString())}
+*Recovered Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.recovered.toString())}
+*Critical Cases:* ${messageFormater.addSpaceToBigStringNumber(countryData.cases.critical.toString())}
+*Deaths:* ${messageFormater.addSpaceToBigStringNumber(countryData.deaths.total.toString())}
+      `
+    }).join('\n\n')
+
+    return `
+*COVID-19 - TOP 5 most affected countries*
+
+${messageBody}
+*Updated @* ${dateTime}
+    `
+  }
 }
 
 export default MessageService
