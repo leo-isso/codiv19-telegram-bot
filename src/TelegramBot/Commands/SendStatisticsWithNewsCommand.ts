547875cc54c5e371ec223726c720c57034913e32
Leonardo Isso <leoisso.work@gmail.com>
2020-04-07 02:09:51 -0300

    Test createButtons

--- src/TelegramBot/Commands/SendStatisticsWithNewsCommand.ts
+++ src/TelegramBot/Commands/SendStatisticsWithNewsCommand.ts
@@ -1,13 +1,16 @@
 import CommandInterface from './CommandInterface'
 import MessageCommandInterface from './MessageCommandInterface'
+import ButtonsCommandInterface from './ButtonsCommandInterface'
 
 import { TelegramBotReceiver } from '../Receivers'
 import RequestMaker from '../../Requests/RequestMakers'
 import StatisticsService from '../../Services/StatisticsService'
 import MessageService from '../../Services/MessageService/MessageService'
-import { CovidStatisticsRequester } from '../../Requests/Requester'
+import { CovidStatisticsRequester, NewsRequester } from '../../Requests/Requester'
+import { StatisticsSchema, NewsSchema } from '../../Schemas'
+import { InlineKeyboardButton } from 'node-telegram-bot-api'
 
-class SendStatisticsWithNewsCommand implements CommandInterface, MessageCommandInterface {
+class SendStatisticsWithNewsCommand implements CommandInterface, MessageCommandInterface, ButtonsCommandInterface {
   public telegramBot: TelegramBotReceiver
 
   constructor (telegramBot: TelegramBotReceiver) {
@@ -16,12 +19,13 @@ class SendStatisticsWithNewsCommand implements CommandInterface, MessageCommandI
 
   async execute ():Promise<void> {
     const message = this.createMessage()
-    this.telegramBot.sendMessage(message)
+    const buttons = this.createButtons()
+    this.telegramBot.sendMessage(message, buttons)
   }
 
   async createMessage (): Promise<string> {
     const statisticRequester = CovidStatisticsRequester
-    const statisticRequest = new RequestMaker(statisticRequester, '/statistics')
+    const statisticRequest = new RequestMaker<StatisticsSchema>(statisticRequester, '/statistics')
     await statisticRequest.makeRequest()
     const payload = statisticRequest.getResponseData()
 
@@ -30,6 +34,15 @@ class SendStatisticsWithNewsCommand implements CommandInterface, MessageCommandI
     const message = MessageService.createStatisticsMessage(statisticData)
 
     return message
+  }
+
+  async createButtons (): Promise<InlineKeyboardButton[][]> {
+    const newsRequester = NewsRequester
+    const newsRequest = new RequestMaker<NewsSchema>(newsRequester, '/top-headlines', { country: 'us', q: 'COVID-19' })
+    await newsRequest.makeRequest()
+    const buttons: InlineKeyboardButton[][] = newsRequest.getResponseData().articles.slice(0, 3).map(article => { return [{ text: article.title, url: article.url }] })
+    console.log(buttons)
+    return buttons
   }
 }
 
