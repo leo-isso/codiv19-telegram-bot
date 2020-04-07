839406a0cf9e5cd7db059ffd0f1c5f76e4bfb4f5
Leonardo Isso <leoisso.work@gmail.com>
2020-04-07 00:25:18 -0300

    Create CovidStatisticsRequester implementation

---
+++ src/Requests/Requester/CovidStatisticsRequester.ts
@@ -0,0 +1,23 @@
+import { AxiosInstance } from 'axios'
+import { OutgoingHttpHeaders } from 'http'
+
+import Requester from './Requester'
+
+class CovidStatisticsRequester extends Requester {
+  apiHost: string = process.env.RAPID_API_HOST
+  apiKey: string = process.env.RAPID_API_KEY
+  apiUrl: string = process.env.RAPID_API_URL
+  instance: AxiosInstance = null
+
+  createHeaders (): OutgoingHttpHeaders {
+    return {
+      'content-type': 'application/octet-stream',
+      'x-rapidapi-host': this.apiHost,
+      'x-rapidapi-key': this.apiKey
+    }
+  }
+}
+
+const CovidStatisticsRequesterInstance = new CovidStatisticsRequester()
+
+export default CovidStatisticsRequesterInstance
