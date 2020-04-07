b48e2d3de41ec151e9f5686fe694ce837a978eb9
Leonardo Isso <leoisso.work@gmail.com>
2020-04-07 01:42:26 -0300

    Export Schemas

--- src/Schemas/index.ts
+++ src/Schemas/index.ts
@@ -1,7 +1,11 @@
 import CountrySchema from './CountrySchema'
 import StatisticsSchema from './StatisticsSchema'
+import NewsSchema from './NewsSchema'
+import ArticleSchema from './ArticleSchema'
 
 export {
+  ArticleSchema,
   CountrySchema,
+  NewsSchema,
   StatisticsSchema
 }
