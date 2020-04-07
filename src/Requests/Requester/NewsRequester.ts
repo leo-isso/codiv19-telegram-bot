9de2853dd706dcad8b887a2df369d88434a8fbfb
Leonardo Isso <leoisso.work@gmail.com>
2020-04-07 00:36:18 -0300

    Add NewsRequester

---
+++ src/Requests/Requester/NewsRequester.ts
@@ -0,0 +1,21 @@
+import { AxiosInstance } from 'axios'
+import { OutgoingHttpHeaders } from 'http'
+
+import Requester from './Requester'
+
+class NewsRequester extends Requester {
+  apiHost: string = process.env.RAPID_API_HOST
+  apiKey: string = process.env.RAPID_API_KEY
+  apiUrl: string = process.env.RAPID_API_URL
+  instance: AxiosInstance = null
+
+  createHeaders (): OutgoingHttpHeaders {
+    return {
+      'X-Api-Key': this.apiKey
+    }
+  }
+}
+
+const NewsRequesterInstance = new NewsRequester()
+
+export default NewsRequesterInstance
