276b10880dc4a3180a033f6d7ddbb175c42a21b8
Leonardo Isso <leoisso.work@gmail.com>
2020-04-13 08:41:47 -0300

    Add CovidStatisticsRequester tests

--- src/tests/Requests/Requester.test.ts
+++ src/tests/Requests/Requester.test.ts
@@ -1,12 +1,23 @@
 import { expect } from 'chai'
 import 'mocha'
 
-import { CovidStatisticsRequester } from './../../Requests/Requester'
+import { CovidStatisticsRequester, NewsRequester } from './../../Requests/Requester'
 
 describe('Testing CovidStatisticsRequester', () => {
   it('CovidStatisticsRequester A and B should represent same instance', () => {
     const RequesterA = CovidStatisticsRequester.getInstance()
     const RequesterB = CovidStatisticsRequester.getInstance()
+
+    const comparizon = (RequesterA === RequesterB)
+
+    expect(comparizon).to.equal(true)
+  })
+})
+
+describe('Testing CovidStatisticsRequester', () => {
+  it('CovidStatisticsRequester A and B should represent same instance', () => {
+    const RequesterA = NewsRequester.getInstance()
+    const RequesterB = NewsRequester.getInstance()
 
     const comparizon = (RequesterA === RequesterB)
 
