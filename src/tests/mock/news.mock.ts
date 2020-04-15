65f7589941b34ed9afa64ef1764a467f4ce59c65
Leonardo Isso <leoisso.work@gmail.com>
2020-04-14 23:06:11 -0300

    Add news mock

---
+++ src/tests/mock/news.mock.ts
@@ -0,0 +1,266 @@
+export default {
+  status: 'ok',
+  totalResults: 321,
+  articles: [
+    {
+      source: {
+        id: 'nhl-news',
+        name: 'NHL News'
+      },
+      author: 'Tom Gulitti',
+      title: 'Arizona Coyotes season snapshot',
+      description: "With the NHL pausing the 2019-20 regular season on March 12 due to the concerns surrounding the coronavirus, NHL.com is taking stock of each of the League's 31 teams. Today, a snapshot of the Arizona Coyotes at the pause.",
+      url: 'https://www.nhl.com/news/2019-20-arizona-coyotes-review/c-316167780',
+      urlToImage: 'https://nhl.bamcontent.com/images/photos/316169270/1024x576/cut.jpg',
+      publishedAt: '2020-04-15T04:00:00Z',
+      content: 'Arizona Coyotes\r\nRecord: 33-29-8, 74 points\r\nPlayoff position: Four points behind Vancouver Canucks and Nashville Predators for second wild card into Stanley Cup Playoffs from Western Conference\r\nThe Coyotes appeared ready to emerge as a Stanley Cup favorite … [+3177 chars]'
+    },
+    {
+      source: {
+        id: 'nhl-news',
+        name: 'NHL News'
+      },
+      author: 'Adam Kimelman',
+      title: 'Favorite memory of season: European adventure with NHL Global Series',
+      description: 'The NHL season was paused March 12 due to concerns surrounding the coronavirus. During the break in the action, NHL.com writers will each look back at his or her favorite memory of the season so far. Today, deputy managing editor Adam Kimelman writes about a …',
+      url: 'https://www.nhl.com/news/favorite-memory-2019-20-season-nhl-global-series/c-316597050',
+      urlToImage: 'https://nhl.bamcontent.com/images/photos/309756948/1024x576/cut.jpg',
+      publishedAt: '2020-04-15T04:00:00Z',
+      content: 'The NHL season was paused March 12 due to concerns surrounding the coronavirus. During the break in the action, NHL.com writers will each look back at his or her favorite memory of the season so far. Today, deputy managing editor Adam Kimelman writes about a … [+3646 chars]'
+    },
+    {
+      source: {
+        id: 'news-com-au',
+        name: 'News.com.au'
+      },
+      author: 'https://www.news.com.au/world/coronavirus/australia/coronavirus-australia-live-updates/live-coverage/c48a7bca7507f7b8a17f5c261b684347',
+      title: 'Coronavirus Australia live updates: IMF predicts worst recession since Great Depression',
+      description: 'The Australian economy will take a hefty hit this year as the world suffers its worst recession since the Great Depression, the International Monetary Fund (IMF) says.',
+      url: 'https://www.news.com.au/world/coronavirus/australia/coronavirus-australia-live-updates/live-coverage/c48a7bca7507f7b8a17f5c261b684347',
+      urlToImage: 'https://content.api.news/v3/images/bin/27878b783dbe4db6c51ea75b7bbf6795',
+      publishedAt: '2020-04-15T02:03:56Z',
+      content: 'The Australian economy will take a hefty hit this year as the world suffers its worst recession since the Great Depression, the International Monetary Fund (IMF) says.“This is a truly global crisis as no country is spared,” IMF chief economist Gita ­Gopinath … [+471 chars]'
+    },
+    {
+      source: {
+        id: 'financial-times',
+        name: 'Financial Times'
+      },
+      author: null,
+      title: 'Coronavirus latest: Trump orders US to halt funding to WTO',
+      description: 'Covid-19 deaths in Italy, one of the countries most affected by the pandemic, surpass 21,000 as more than 600 people die in 24 hours',
+      url: 'https://www.ft.com/content/59ff62ae-3f66-3ec2-b269-e94e9a72f198',
+      urlToImage: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fblogs.ft.com%2Fthe-world%2Ffiles%2F2020%2F04%2FTrumpTuesday.jpg?source=next-opengraph&fit=scale-down&width=900',
+      publishedAt: '2020-04-15T01:52:55.9810056Z',
+      content: "Fed's Bullard calls for widespread testing to get economy back on track\r\nJames Politi in Washington\r\nA senior Federal Reserve official has called for “widespread testing” to allow economic activity to return to normal after the coronavirus pandemic eases its … [+1709 chars]"
+    },
+    {
+      source: {
+        id: 'reddit-r-all',
+        name: 'Reddit /r/all'
+      },
+      author: 'https://www.facebook.com/bbcnews',
+      title: 'US to halt funding to WHO over coronavirus',
+      description: 'The World Health Organization "failed in its basic duty" over coronavirus, Donald Trump says.',
+      url: 'https://www.bbc.com/news/world-us-canada-52289056?at_medium=custom7&amp;at_campaign=64&amp;at_custom2=twitter&amp;at_custom1=%5Bpost+type%5D&amp;at_custom3=%40BBCBreaking&amp;at_custom4=FF6CAAE8-7E9F-11EA-A0A9-EBD0923C408C',
+      urlToImage: 'https://ichef.bbci.co.uk/images/ic/1024x576/p089hx0q.jpg',
+      publishedAt: '2020-04-15T01:52:54.4024921Z',
+      content: 'Media captionTrump said the WHO had "failed in its basic duty"\r\nUS President Donald Trump says he has instructed his administration to halt funding to the World Health Organization (WHO).\r\nHe said the WHO had "failed in its basic duty" in its response to the … [+4945 chars]'
+    },
+    {
+      source: {
+        id: 'google-news-au',
+        name: 'Google News (Australia)'
+      },
+      author: null,
+      title: 'Victorian Government announces $500m package for private rental sector',
+      description: 'Victorian Premier Daniel Andrews has announced a $500 million package to assist landlords and tenants during the coronavirus pandemic.',
+      url: 'https://www.abc.net.au/news/2020-04-15/coronavirus-victoria-landlords-renters-assistance-for-pandemic/12149210',
+      urlToImage: 'https://www.abc.net.au/news/image/12080478-16x9-700x394.jpg',
+      publishedAt: '2020-04-15T01:49:04+00:00',
+      content: 'Updated \r\nApril 15, 2020 11:33:34\r\nVictorian Premier Daniel Andrews has announced a $500 million package to assist landlords and tenants during the coronavirus pandemic.\r\nThe package will include $420 million in land tax relief for landlords who provide tenan… [+2800 chars]'
+    },
+    {
+      source: {
+        id: 'cbs-news',
+        name: 'CBS News'
+      },
+      author: 'CBS News',
+      title: 'GM CEO says automaker is ramping up ventilator production',
+      description: 'General Motors, with the help of Ventec Life Systems, has begun producing ventilators to help in the fight against coronavirus. Mary Barra, the CEO of GM, and Chris Kriple, CEO of Ventec Life Systems, discussed their ongoing efforts with "CBS Evening News" an…',
+      url: 'https://www.cbsnews.com/video/gm-ceo-says-automaker-is-ramping-up-ventilator-production/',
+      urlToImage: 'https://cbsnews2.cbsistatic.com/hub/i/r/2020/04/15/3a708215-c181-4bb7-8fdf-97aa5496b929/thumbnail/1200x630/90a6e1bee2d4e63dc629994e1c454911/0414-en-gmventec-2055469-640x360.jpg',
+      publishedAt: '2020-04-15T01:44:39+00:00',
+      content: 'Watch CBSN Live\r\nCopyright © 2020 CBS Interactive Inc. All rights reserved.'
+    },
+    {
+      source: {
+        id: 'google-news-au',
+        name: 'Google News (Australia)'
+      },
+      author: null,
+      title: 'Coronavirus Australia live updates: Trump halts funding to WHO over virus response - The Australian',
+      description: 'Subscribe to The Australian to get unrestricted digital access, home paper delivery, Apps for iPad and Android, member only +Rewards and much more...',
+      url: 'https://www.theaustralian.com.au/subscribe/news/1/',
+      urlToImage: null,
+      publishedAt: '2020-04-15T01:38:00+00:00',
+      content: 'Subscribe to The Australian to get unrestricted digital access, home paper delivery, Apps for iPad and Android, member only +Rewards and much more...'
+    },
+    {
+      source: {
+        id: 'fox-news',
+        name: 'Fox News'
+      },
+      author: 'Fox News',
+      title: 'Tucker: Some lawmakers are trying to exploit the coronavirus pandemic',
+      description: "For some in our political class, the COVID-19 crisis isn't a human tragedy, it's an opportunity.",
+      url: 'https://video.foxnews.com/v/6149442988001/',
+      urlToImage: 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/4b0023e0-1b8b-4bc5-baa5-696f2a72839a/63861d6e-240f-45bc-90c4-3735f9a3f3dd/1280x720/match/image.jpg',
+      publishedAt: '2020-04-15T01:37:58.9776243Z',
+      content: null
+    },
+    {
+      source: {
+        id: 'fox-news',
+        name: 'Fox News'
+      },
+      author: 'Fox News',
+      title: "Rita Wilson opens up about coronavirus symptoms, warns about 'extreme' chloroquine side effects",
+      description: "Rita Wilson and her husband Tom Hanks were the first celebrities to reveal they tested positive for the coronavirus last month. Now, the singer and actress is opening up in greater detail about the pair's symptoms and is sharing a warning about the controvers…",
+      url: 'https://www.foxnews.com/entertainment/rita-wilson-coronavirus-symptoms-warns-chloroquine-side-effects',
+      urlToImage: 'https://static.foxnews.com/foxnews.com/content/uploads/2020/03/Hanks-Wilson.jpg',
+      publishedAt: '2020-04-15T01:37:57.6283302Z',
+      content: 'Get all the latest news on coronavirus and more delivered daily to your inbox.  Sign up here.  \r\nRita Wilson and her husband Tom Hanks were the first celebrities to reveal they tested positive for the coronavirus last month. Now, the singer and actress has op… [+4090 chars]'
+    },
+    {
+      source: {
+        id: 'fox-news',
+        name: 'Fox News'
+      },
+      author: 'Fox News',
+      title: "Trump says 'scariest day of my life' was being told US was short on ventilators",
+      description: 'President Trump on Tuesday called it the "scariest day" of his life when he had learned the United States might not have enough ventilators to fight the novel coronavirus.',
+      url: 'https://www.foxnews.com/politics/trump-scariest-day-being-told-us-short-ventilators',
+      urlToImage: 'https://static.foxnews.com/foxnews.com/content/uploads/2020/04/AP20101596418280.jpg',
+      publishedAt: '2020-04-15T01:37:55.2540535Z',
+      content: 'Get all the latest news on coronavirus and more delivered daily to your inbox. Sign up here.\r\nPresident Trump on Tuesday called it the "scariest day" of his life when he had learned the United States might not have enough ventilators to fight the novel corona… [+1561 chars]'
+    },
+    {
+      source: {
+        id: 'reuters',
+        name: 'Reuters'
+      },
+      author: 'Deena Beasley',
+      title: 'New York City hospitals cancel temporary workers as coronavirus cases stabilize',
+      description: 'Staffing agencies, which have deployed thousands of healthcare workers in recent weeks to jobs at hospitals in New York City and other areas hit hard by the coronavirus, say some of those temporary workers are no longer needed.',
+      url: 'http://feeds.reuters.com/~r/reuters/topNews/~3/36rmyTX6L2M/new-york-city-hospitals-cancel-temporary-workers-as-coronavirus-cases-stabilize-idUSKCN21X00X',
+      urlToImage: 'https://s1.reutersmedia.net/resources/r/?m=02&d=20200415&t=2&i=1515098220&w=1200&r=LYNXNPEG3E00A',
+      publishedAt: '2020-04-15T01:37:46Z',
+      content: '(Reuters) - Staffing agencies, which have deployed thousands of healthcare workers in recent weeks to jobs at hospitals in New York City and other areas hit hard by the coronavirus, say some of those temporary workers are no longer needed. \r\nThe trend, couple… [+4007 chars]'
+    },
+    {
+      source: {
+        id: 'google-news-fr',
+        name: 'Google News (France)'
+      },
+      author: '20 Minutes avec AFP',
+      title: "Coronavirus : Donald Trump suspend le financement américain de l'OMS",
+      description: 'Le président américain a accusé l’Organisation mondiale de la santé d’avoir pris pour argent comptant les affirmations de la Chine au début de l’épidémie',
+      url: 'https://www.20minutes.fr/monde/2760847-20200415-coronavirus-donald-trump-suspend-financement-americain-oms',
+      urlToImage: 'https://img.20mn.fr/KcpxSOapTmWVxMxhIvbMmA/648x360_donald-trump-fait-point-coronavirus-depuis-jardins-maison-blanche-14-avril-2020.jpg',
+      publishedAt: '2020-04-15T01:37:15+00:00',
+      content: 'Donald Trump fait le point sur le coronavirus depuis les jardins de la Maison Blanche, le 14 avril 2020. Alex Brandon/AP/SIPA \r\nIl a trouvé un coupable. Donald Trump a annoncé mardi la suspension de la contribution américaine à lOrganisation mondiale de la sa… [+3258 chars]'
+    },
+    {
+      source: {
+        id: 'abc-news-au',
+        name: 'ABC News (AU)'
+      },
+      author: null,
+      title: 'Trump says US will halt funding to WHO over handling of coronavirus pandemic',
+      description: 'The US President accuses the WHO of promoting Chinese disinformation about the virus, claiming it failed in its basic duty and it must be held accountable.',
+      url: 'http://www.abc.net.au/news/2020-04-15/trump-halts-who-funding-coronavirus/12149142',
+      urlToImage: 'https://www.abc.net.au/news/image/12149394-16x9-700x394.jpg',
+      publishedAt: '2020-04-15T01:29:56Z',
+      content: 'Updated \r\nApril 15, 2020 11:29:56\r\nUS President Donald Trump says he has told his administration to halt funding to the World Health Organisation over its handling of the coronavirus pandemic.\r\nKey Points:\r\n<ul><li>US President Donald Trump lashed out at the … [+4686 chars]'
+    },
+    {
+      source: {
+        id: 'cbs-news',
+        name: 'CBS News'
+      },
+      author: 'christina capatides',
+      title: "Pediatrician says 80% of kids likely have coronavirus, but they're so asymptomatic you'd never know",
+      description: "\"If your child does have a low-grade fever right now and a cold, you have to assume that it's COVID.\"",
+      url: 'https://www.cbsnews.com/news/coronavirus-children-pediatrician-80-percent-asymptomatic/',
+      urlToImage: 'https://cbsnews3.cbsistatic.com/hub/i/r/2020/04/14/5e627e85-b3dc-4eab-aca0-0c48f0138b71/thumbnail/1200x630/8b7067b7d932d3356c7eb4ac673df360/gettyimages-1097508168.jpg',
+      publishedAt: '2020-04-15T01:29:48+00:00',
+      content: "The elderly and immunocompromised are most at risk of severe coronavirus complications, but over the last several weeks there's been growing evidence that younger, otherwise healthy people can also fall victim to the deadly virus. The Centers for Disease Cont… [+5329 chars]"
+    },
+    {
+      source: {
+        id: 'cnn-es',
+        name: 'CNN Spanish'
+      },
+      author: 'CNN',
+      title: 'Al menos 27 niños migrantes bajo custodia de EE.UU. resultaron positivos por coronavirus',
+      description: 'Seis de los 27 menores que dieron positivo por covid-19 se han recuperado, mientras los otros están en aislamiento médico en dos instalaciones de Texas y una de Illinois, de acuerdo a la Oficina de…',
+      url: 'https://cnnespanol.cnn.com/2020/04/14/al-menos-27-ninos-migrantes-bajo-custodia-de-ee-uu-resultaron-positivos-por-coronavirus/',
+      urlToImage: 'https://cnnespanol2.files.wordpress.com/2020/04/nincc83os-migrantes-coronavirus.jpg?quality=100&strip=info',
+      publishedAt: '2020-04-15T01:27:48Z',
+      content: 'Ordenan liberación de menores migrantes por covid-19 2:16\r\n(CNN) Al menos 27 niños migrantes que se encuentran bajo custodia del gobierno de Estados Unidos han resultado positivos por coronaravirus hasta este lunes, de acuerdo con la última actualización de l… [+2704 chars]'
+    },
+    {
+      source: {
+        id: 'reuters',
+        name: 'Reuters'
+      },
+      author: 'Jeff Mason',
+      title: 'Trump halts World Health Organization funding amid coronavirus pandemic',
+      description: 'President Donald Trump said on Tuesday he would halt funding to the World Health Organization over its handling of the coronavirus pandemic while his administration reviews its response to the global crisis.',
+      url: 'http://feeds.reuters.com/~r/reuters/topNews/~3/FpYQZSNBfN4/trump-halts-world-health-organization-funding-amid-coronavirus-pandemic-idUSKCN21W34Y',
+      urlToImage: 'https://s2.reutersmedia.net/resources/r/?m=02&d=20200414&t=2&i=1515086704&w=1200&r=LYNXNPEG3D26B',
+      publishedAt: '2020-04-15T01:25:05Z',
+      content: 'WASHINGTON (Reuters) - President Donald Trump said on Tuesday he would halt funding to the World Health Organization over its handling of the coronavirus pandemic while his administration reviews its response to the global crisis. \r\nTrump, at a White House ne… [+4906 chars]'
+    },
+    {
+      source: {
+        id: 'fox-news',
+        name: 'Fox News'
+      },
+      author: 'Fox News',
+      title: 'Italy records fewest new coronavirus cases in a month',
+      description: 'The number of new coronavirus cases in Italy has fallen Tuesday to its lowest level in a month.',
+      url: 'https://www.foxnews.com/world/italy-records-fewest-new-coronavirus-cases-in-month',
+      urlToImage: 'https://cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/a9056e51-994c-4187-ab16-5012c8a8d117/fe61a36a-4368-4244-b5e2-9500d6fa28dc/1280x720/match/image.jpg',
+      publishedAt: '2020-04-15T01:22:44.8891486Z',
+      content: 'Get all the latest news on coronavirus and more delivered daily to your inbox. Sign up here. \r\nThe number of new coronavirus cases in Italy fell Tuesday to its lowest level in a month.\r\nItaly’s civil protection agency is reporting 2,972 new cases of COVID-19 … [+616 chars]'
+    },
+    {
+      source: {
+        id: 'google-news-ar',
+        name: 'Google News (Argentina)'
+      },
+      author: 'Clarín.com',
+      title: 'Coronavirus en Argentina: Alberto Fernández se reunió con Máximo Kirchner para analizar el "impuesto a los',
+      description: 'Participaron también el ministro de Economía, Martín Guzmán, y el diputado Carlos Heller.',
+      url: 'https://www.clarin.com/politica/coronavirus-argentina-alberto-fernandez-reunio-maximo-kirchner-analizar-impuesto-ricos-_0_IIaGGFJFO.html',
+      urlToImage: 'https://images.clarin.com/2020/04/14/el-presidente-alberto-fernandez-junto___SkjKxvW_e_1200x630__1.jpg',
+      publishedAt: '2020-04-15T01:22:23+00:00',
+      content: 'El presidente Alberto Fernández se reunió en la tarde de este martes con el presidente del bloque del Frente de Todos en Diputados, Máximo Kirchner; con el presidente de la comisión de Presupuesto, Carlos Heller, y con el ministro de Economia, Martín Guzmán. … [+2757 chars]'
+    },
+    {
+      source: {
+        id: 'the-irish-times',
+        name: 'The Irish Times'
+      },
+      author: 'Paul Cullen',
+      title: 'Coronavirus: Holohan calls for quicker deaths registration',
+      description: '‘More timely’ assessment will help create more complete picture of virus impact',
+      url: 'https://www.irishtimes.com/\t\t\t\t\t\t\t/news/politics/coronavirus-holohan-calls-for-quicker-deaths-registration-1.4229029\t',
+      urlToImage: 'https://www.irishtimes.com/image-creator/?id=1.4229028&origw=1440',
+      publishedAt: '2020-04-15T01:17:12Z',
+      content: 'Changes are to be made to the way deaths involving Covid-19 are reported so that a fuller picture of the impact of the virus, especially in nursing homes, can be established.\r\nThe normal three-month period in which deaths can be registered needs to change in … [+2543 chars]'
+    }
+  ]
+}
